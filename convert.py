90427c8725d6fbf51be46362440447a80a25174c
Özgür Yurt <93544343+ozguryurt@users.noreply.github.com>
2024-12-10 21:22:45 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
+# https://www.kaggle.com/datasets/winvoker/turkishsentimentanalysisdataset adresindeki veri setinin sütun isimlerini düzenlemek için kullanılan kod.
 import pandas as pd
-df = pd.read_csv('yeni_veriler2.csv')
+df = pd.read_csv('veri_seti.csv')
 df = df.rename(columns={'text': 'comment'})
 df['label'] = df['label'].replace({'Positive': 'pozitif', 'Negative': 'negatif', 'Notr': 'nötr'})
 df = df.drop(columns=['dataset'])
